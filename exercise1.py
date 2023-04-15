6732642eb6a3c2146cfcf2b8c18b9a893866bab6
SawNoah <129855458+SawNoah@users.noreply.github.com>
2023-04-15 09:37:09 +0630

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,10 +1,15 @@
 import streamlit as st
+from PIL import Imaage
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
 
 
-img = st.read('image/a1.jpg')
-gray = st.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = Image.open("image/a1.jpg")
+st.image(img,width=200)
 
-st.imshow('Original image', img)
-st.imshow('Gray image', gray)
+if st.checkbox("Show/Hide"):
+  st.text("Showing the widget")
+  st.image(img,width=50)
+
+status = st.radio("select gender:", ('Male','Female'))
+st.success(status)
