317b08288924eac94569d393fb0f45308ec7154f
SawNoah <129855458+SawNoah@users.noreply.github.com>
2023-04-15 09:27:56 +0630

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,10 @@
 import streamlit as st
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
+
+import cv2
+img = cv2.imread('image/a1.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Original image', img)
+cv2.imshow('Gray image', gray)
