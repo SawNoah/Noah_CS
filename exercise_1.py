376eebbaff05d7ff2b8ace3766586aceaea8e1ce
SawNoah <129855458+SawNoah@users.noreply.github.com>
2023-04-18 20:35:25 +0630

    Update exercise_1.py

--- exercise_1.py
+++ exercise_1.py
@@ -36,3 +36,35 @@ st.write("Text with write")
 # This is not possible using st.text().
 #Writing python inbuilt function range()
 st.write(range(10))
+
+#Stream Basic Function
+import streamlit as st
+from PIL import Image
+
+#Display Images:
+img = Image.open("image/Lenna.png")
+
+#display image using streamlit
+#width is used to set the width of an image
+st.image(img, width=200)
+
+#checkbox
+#A checkbox returns a boolean value.
+#When the box is checked, it returns a True value
+#else returns a Flase value.
+
+#check if the checkbox is checked
+# title of the checkbox is "Show/Hide"
+if st.checkbox("Show/Hide"):
+  #display the text if the checkbox returns True value
+  st.text("Showing the widget")
+  st.image(img,width=50)
+  
+#radio button
+#first rgument is the title of the radio button
+#second argument is the options for the radio button
+status = st.radio("Select Gender:", ('Male',"Female"))
+#conditional statement to print
+#Male if male is selected else print female
+#show the result using the success function
+st.success(status)
